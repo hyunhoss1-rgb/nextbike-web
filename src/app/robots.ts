bc69ad2eab7036afd6aec7714316661ab496eda6
NextBike <contact@nextbike.kr>
2026-09-16 21:52:41 +0900

    seo: 네이버 웹마스터도구 Yeti 크롤러 명시적 수집 허용 규칙 추가

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,16 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "Yeti",
+        allow: "/",
+      },
+    ],
     sitemap: "https://www.xn--b60bj1s89e3pf91mzkd.com/sitemap.xml",
   };
 }

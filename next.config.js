5fc0941f6c15555c1f91fb13aa664e4abb57006e
NextBike <contact@nextbike.kr>
2026-09-16 22:26:53 +0900

    feat: nextbike.kr 접속 시 전국중고오토바이매입.kr로 308 영구 자동 리다이렉트

--- next.config.js
+++ next.config.js
@@ -4,6 +4,32 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'nextbike.kr',
+          },
+        ],
+        destination: 'https://www.xn--299alk823a88b8ztw1bpdu7bh3ec67a.kr/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.nextbike.kr',
+          },
+        ],
+        destination: 'https://www.xn--299alk823a88b8ztw1bpdu7bh3ec67a.kr/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
